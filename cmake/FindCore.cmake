5188c280859dbb971a95ceee7eb0f7d90c8c8aeb
lucaisacoder <lucas_hsueh@hotmail.com>
2020-01-28 20:57:38 +0800

    build cmake framework
    Change-Id: I007760b5c37c0a2653a0d8a670f92cc91e167684

---
+++ cmake/FindCore.cmake
@@ -0,0 +1,24 @@
+set(Core_ROOT_DIRS ${PROJ_REPO_TOP_DIR}/modules/core)
+
+set(_WINDOWS FALSE)
+if("${CMAKE_SYSTEM_NAME}" MATCHES "Windows")
+    set(_WINDOWS TRUE)
+endif()
+
+set(_LINUX FALSE)
+if("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
+    set(_LINUX TRUE)
+endif()
+
+find_path(Core_INCLUDE_DIR
+    NAMES "core_api.h"
+    PATHS "${Core_ROOT_DIRS}/*")
+
+find_library(Core_LIBRARIES
+    NAMES "libcore.a"
+    HINTS "${Core_ROOT_DIRS}/output")
+
+if(Core_INCLUDE_DIR)
+    set(Core_FOUND true)
+    set(Core_INCLUDE_DIRS ${Core_INCLUDE_DIR})
+endif()
