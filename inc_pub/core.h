287b4122676a40d715f39fb8d03c1fac64c70ca9
lucaisacoder <lucas_hsueh@hotmail.com>
2020-01-28 15:08:49 +0800

    add cmakelists.txt
    Change-Id: I68481226fb47de12a122338ff80743cc8db9fac3

---
+++ inc_pub/core.h
@@ -0,0 +1,6 @@
+#ifndef CORE_API_H_H
+#define CORE_API_H_H
+
+int core_main(void);
+
+#endif //CORE_API_H_H
--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include "osal_api.h"
 #include "app_api.h"
 
-int main(void)
+int core_main(void)
 {
 	printf("%s, %s: PUBLIC_MACRO=%d\n", __FILE__, __FUNCTION__, PUBLIC_MACRO);
 	osal_api();
